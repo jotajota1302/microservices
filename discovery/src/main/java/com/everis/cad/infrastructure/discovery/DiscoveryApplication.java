3d27eab26350d9129d1f9f9be6661eccd5974fc1
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-04-03 18:42:43 +0200

    inlcude admin server into discovery

--- discovery/src/main/java/com/everis/cad/infrastructure/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/everis/cad/infrastructure/discovery/DiscoveryApplication.java
@@ -2,10 +2,15 @@ package com.everis.cad.infrastructure.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
+@EnableDiscoveryClient
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
