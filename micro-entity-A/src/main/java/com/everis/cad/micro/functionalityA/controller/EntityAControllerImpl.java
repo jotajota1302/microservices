b65c1b7a2a9a3e7bf06ea5271ee6a03469d80222
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-04-03 12:05:46 +0200

    fix pom versions in compiler plugin

--- micro-entity-A/src/main/java/com/everis/cad/micro/functionalityA/controller/EntityAControllerImpl.java
+++ micro-entity-A/src/main/java/com/everis/cad/micro/functionalityA/controller/EntityAControllerImpl.java
@@ -1,15 +1,17 @@
 package com.everis.cad.micro.functionalityA.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 import com.everis.cad.micro.commons.AbstractMicroserviceController;
 import com.everis.cad.micro.commons.Manager;
 import com.everis.cad.micro.functionalityA.dto.EntityADto;
 import com.everis.cad.micro.functionalityA.service.EntityAService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-class EntityAControllerImpl extends AbstractMicroserviceController<EntityADto, Integer> implements EntityAController {
+public class EntityAControllerImpl extends AbstractMicroserviceController<EntityADto, Integer> implements EntityAController {
 
     private final EntityAService entityAService;
 
