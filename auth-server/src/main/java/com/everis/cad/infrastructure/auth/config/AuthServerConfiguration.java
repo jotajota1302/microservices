9abbb1db3d1063cd132177a5c9b4152a90be5be9
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-04-03 20:35:06 +0200

    remove admin

--- auth-server/src/main/java/com/everis/cad/infrastructure/auth/config/AuthServerConfiguration.java
+++ auth-server/src/main/java/com/everis/cad/infrastructure/auth/config/AuthServerConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,16 +13,18 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
+
 @Configuration
 public class AuthServerConfiguration
         extends WebSecurityConfigurerAdapter
         implements AuthorizationServerConfigurer {
 
-    @Bean
+
+	@Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManager();
     }
-
+   
     @Autowired
     AuthenticationManager authenticationManager;
 
